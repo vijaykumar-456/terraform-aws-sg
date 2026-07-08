4e36fccf4a60b0135d161065f3e6c0b3631e553e
Vk167 <viijaykumarr07@gmail.com>
2026-07-08 08:04:19 +0530

    sg module

--- locals.tf
+++ locals.tf
@@ -1,9 +1,9 @@
 locals {
-  common_name = "${var.project}-${var.environment}-${var.component}"
+  common_name = "${var.project}-${var.environment}-${var.sg_name}"
   common_tags = {
     project = var.project,
     environment = var.environment,
-    component = var.component,
+    component = var.sg_name,
     Name = local.common_name
   }
 }
